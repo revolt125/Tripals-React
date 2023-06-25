5775d02fd4dd5d9298881341d7a9a72dea3aca8c
revolt125 <revolt125@gmail.com>
2023-06-26 01:36:47 +0800

    Update config.js

--- src/assets/config.js
+++ src/assets/config.js
@@ -1,3 +1,3 @@
 // 這個是放 後端API 的 URL
-// export const baseUrl = "http://localhost:8000";
-export const baseUrl = "https://tripals-backend-production.up.railway.app";
+export const baseUrl = "http://localhost:8000";
+// export const baseUrl = "https://tripals-backend-production.up.railway.app";
